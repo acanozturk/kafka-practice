d186e50edcc240b6b4a6353cd5cb98099ad0a8e1
Ali Can Öztürk <acanozturk96@gmail.com>
2021-07-08 11:45:35 +0300

    initialized kafka-streams word count project

---
+++ kafka-streams-2/src/main/java/com/kafkastreams2/WordCount.java
@@ -0,0 +1,13 @@
+package com.kafkastreams2;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+final class WordCount {
+
+    public static void main(String[] args) {
+
+    }
+}
